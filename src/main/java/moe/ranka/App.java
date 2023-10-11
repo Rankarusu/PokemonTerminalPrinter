a9fad7fb4865b58e6ecf82b4f016182de80fe07f
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-10-11 20:35:50 +0200

    call pokeapi to get id from name

--- src/main/java/moe/ranka/App.java
+++ src/main/java/moe/ranka/App.java
@@ -1,12 +1,9 @@
 package moe.ranka;
 
-import java.util.Arrays;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
         var client = new HttpClient();
-        var res = client.fetchIcon(args[0]);
+        var res = client.getSpriteByIdOrName(args[0]);
         var printer = new ImagePrinter();
         printer.printImage(res);
     }
--- src/main/java/moe/ranka/HttpClient.java
+++ src/main/java/moe/ranka/HttpClient.java
@@ -1,34 +1,80 @@
 package moe.ranka;
 
+import org.json.JSONObject;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
 
 
 public class HttpClient {
-    //TODO: use graphql and pokeapi to get data from names and ids
+    public static final String imgBaseUrl = "https://veekun.com/dex/media/pokemon/icons/";
+    public static final String pokemonBaseUrl = "https://pokeapi.co/api/v2/pokemon-species/"; // takes id or name
 
-    /*
-    * query samplePokeAPIquery {
-  species: pokemon_v2_pokemonspecies(where: {pokemon_v2_pokemons: {id: {_eq: 10}}}) {
-    name
-    id
-  }
-}
-*/
-    public static final String baseUrl = "https://veekun.com/dex/media/pokemon/icons/";
 
-    public BufferedImage fetchIcon(String dexNr) {
-        try {
-
-            URI url = new URI(baseUrl + dexNr + ".png");
-            return ImageIO.read(url.toURL());
-
-        } catch (Exception e) {
-            e.printStackTrace();
+    public BufferedImage getSpriteByIdOrName(String idOrName) {
+        int id;
+        if (idOrName.matches("\\d+")) {
+            id = Integer.parseInt(idOrName);
+        } else {
+            id = this.fetchPokemonId(idOrName);
         }
 
+        return fetchSprite(id);
+    }
 
-        return null;
+    private int fetchPokemonId(String name) {
+
+        URI uri;
+        try {
+            uri = new URI(pokemonBaseUrl + name);
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL()
+                                                                  .openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                connection.disconnect();
+
+                JSONObject json = new JSONObject(response.toString());
+                return Integer.parseInt(json.get("id").toString());
+
+            } else {
+                throw new RuntimeException(
+                        "Did not receive HTTP_OK Response when trying to get pokemon id");
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Something went wrong while fetching the pokemon Id");
+        }
+
+    }
+
+    private BufferedImage fetchSprite(int dexNr) {
+
+        URI uri = null;
+        try {
+            uri = new URI(imgBaseUrl + dexNr + ".png");
+            return ImageIO.read(uri.toURL());
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Something went wrong while fetching the sprite");
+        }
+
     }
 }
